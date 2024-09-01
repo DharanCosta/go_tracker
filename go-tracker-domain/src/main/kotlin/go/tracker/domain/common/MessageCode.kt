5a92c9934353a6420c8b7c31968c9b66f5953de9
Dharan <dharancosta@gmail.com>
2024-08-31 22:50:35 -0300

    GOT-17: Creating new MedalStatusNotFoundException

--- go-tracker-domain/src/main/kotlin/go/tracker/domain/common/MessageCode.kt
+++ go-tracker-domain/src/main/kotlin/go/tracker/domain/common/MessageCode.kt
@@ -4,6 +4,8 @@ data class MessageCode(
     val name: String
 ) {
     companion object {
+        const val TRAINER_NOT_FOUND = "trainer.not.found"
+        const val TRAINER_MEDAL_STATUS_NOT_FOUND = "trainer.medal.status.not.found"
         const val REQUIRED_FIELD = "required.field"
         const val UNIQUE_FIELD_VIOLATION = "unique.field.violation"
         const val INVALID_FIELD = "invalid.field"
